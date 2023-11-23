84127d87eb0bd1f0748557914dfafbb30f2a4baa
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-11-23 10:09:03 +0700

    finish nomor 1

---
+++ akhdani-test/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            int randomNumbers = (int) (Math.random() * 50 + 1);
+
+            if (randomNumbers == randomNumbers) {
+                temp = randomNumbers.eSystem.out.println(randomNumbers + "pertama");
+            }
+        }
+    }
+}
+
+// 1
+// public static void main(String[] args) {
+// System.out.println(luasSegitiga(10, 20));
+// }
+
+// public static int luasSegitiga(int alas, int tinggi) {
+// int luasSegitiga = (alas * tinggi) / 2;
+// return luasSegitiga;
+// }
