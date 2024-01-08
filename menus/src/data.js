61ac7d3931bf04bcc99a67e23f5aa77d88ac2fd8
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-01-08 21:51:20 +0700

    finish react-menus

---
+++ menus/src/data.js
@@ -0,0 +1,48 @@
+const menu = [
+    {
+        id: 1,
+        title: "Buttermilk Pancakes",
+        category: "",
+        price: 15000,
+        img: "./images/uyee.png",
+        desc: "Ad quis pariatur duis qui minim anim proident do tempor incididunt ea incididunt eiusmod culpa.", 
+    },
+
+    {
+        id: 2,
+        title: "Buttermilk Pancakes",
+        category: "Breakfast",
+        price: 15000,
+        img: "./images/ahayy.png",
+        desc: "Sit ea non commodo tempor fugiat veniam ea cupidatat ad aute in quis sint.",
+    },
+
+    {
+        id: 3,
+        title: "Buttermilk Pancakes",
+        category: "Breakfast",
+        price: 15000,
+        img: "./images/ayee.png",
+        desc: "Anim cupidatat laborum et laborum commodo enim in adipisicing.",
+    },
+
+    {
+        id: 4,
+        title: "Buttermilk Pancakes",
+        category: "Breakfast",
+        price: 15000,
+        img: "./images/boy.png",
+        desc: "Esse anim eu commodo ut officia.",
+    },
+
+    {
+        id: 5,
+        title: "Buttermilk Pancakes",
+        category: "Breakfast",
+        price: 15000,
+        img: "./images/uhuyy.jpeg",
+        desc: "Excepteur cillum consectetur nulla magna voluptate qui magna.",
+    },
+];
+
+export default menu;
